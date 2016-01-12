11f9cb08eaa6c44af6e44ec8b48b716812374316
tinoroh <tinorohmund@hotmail.de>
2016-01-12 21:41:36 +0100

    added data and etc handler

--- src/main/testserver.go
+++ src/main/testserver.go
@@ -15,7 +15,29 @@ func mainHandler(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte(responseString))
 }
 
+func dataHandler(w http.ResponseWriter, r *http.Request){
+	q := r.URL.Query()
+	name := q.Get("nam")
+	if name == "" {
+		name = "Planet"
+	}
+	responseString := "<html><body>Hello " + name + "</body></html>"
+	w.Write([]byte(responseString))
+}
+
+func etcHandler(w http.ResponseWriter, r *http.Request){
+	q := r.URL.Query()
+	name := q.Get("nam")
+	if name == "" {
+		name = "System"
+	}
+	responseString := "<html><header><h1>etcHandler</h1></header><body>Hello " + name + "</body></html>"
+	w.Write([]byte(responseString))
+}
+
 func main(){
 	http.HandleFunc("/", mainHandler)
+	http.HandleFunc("/data", dataHandler)
+	http.HandleFunc("/etc", etcHandler)
 	log.Fatalln(http.ListenAndServe(":8080",nil))
 }
