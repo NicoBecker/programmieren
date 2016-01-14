aa1d349b615c3a9ada61d1e013d489d4c7588961
tinoroh <tinorohmund@hotmail.de>
2016-01-14 21:58:01 +0100

    scanner, starting, stopping

--- src/main/testserver.go
+++ src/main/testserver.go
@@ -7,7 +7,6 @@ import (
 	"bufio"
 	"os"
 	"fmt"
-//	"strings"
 )
 
 func mainHandler(w http.ResponseWriter, r *http.Request){
@@ -33,29 +32,43 @@ func dataHandler(w http.ResponseWriter, r *http.Request){
 }
 
 func etcHandler(w http.ResponseWriter, r *http.Request){
-	reader := bufio.NewReader(os.Stdin)
+//	reader := bufio.NewReader(os.Stdin)
+//	fmt.Print("Enter Text: ")
+//	text, _ := reader.ReadString('\n')
+	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("Enter Text: ")
-	text, _ := reader.ReadString('\n')
-	txtfmt := text[0:len(text)-2]
+	for scanner.Scan() {
+		txtfmt := scanner.Text()
+	//	txtfmt := text[0:len(text)-2]
 	if txtfmt == "tasklist" {
 		cmd := exec.Command(txtfmt)
 		stdout, _ := cmd.Output()
 		responseString := "<html><header><h1>etcHandler</h1></header><body>" + string(stdout) + "</body></html>"
 		w.Write([]byte(responseString))
+		fmt.Print("Enter Text: ")
 	} else if txtfmt == "audstart"{
-		cmd := exec.Command("C:\\Program Files (x86)\\Audacity\\audacity")
-		stdout, err := cmd.Output()
-		fmt.Println(err)
-		responseString := "<html><header><h1>etcHandler</h1></header><body>" + string(stdout) + "</body></html>"
+		cmdToRun := "C:\\Program Files (x86)\\Audacity\\audacity.exe"
+		args := []string{"arg1"}
+		procAttr := new(os.ProcAttr)
+		procAttr.Files = [] *os.File{os.Stdin, os.Stdout, os.Stderr}
+		if process, err := os.StartProcess(cmdToRun, args, procAttr); err != nil {
+			fmt.Printf("ERROR Unable to run %s: %s", cmdToRun, err.Error())
+		} else {
+			fmt.Printf("%s running as pid %d", cmdToRun, process.Pid)
+			//process.Kill()
+		}
+		responseString := "<html><header><h1>etcHandler</h1></header><body>Hello</body></html>"
 		w.Write([]byte(responseString))
+		fmt.Print("Enter Text: ")
 	} else if txtfmt == "audstop"{
-	//	cmd:= exec.Command("tasklist")
-//		stdout, _ :=cmd.Output()
+		
+		
 
 		responseString := "<html><header><h1>etcHandler</h1></header><body>" + txtfmt + "</body></html>"
 		w.Write([]byte(responseString))
 	}
-
+	//fmt.Print("Enter Text: ")
+	}
 	
 }
 
